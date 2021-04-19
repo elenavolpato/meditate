e6e051254b72fcc68525574c2845c6b8bf4a5a86
Elena Volpato <talkto@elenavolpato.me>
2021-04-18 23:32:55 -0300

    fixed countdown and stop after time is finished

--- src/views/Meditate.vue
+++ src/views/Meditate.vue
@@ -1,52 +1,110 @@
-<template>
 <!-- 1 bell at the beginnning and 3 at the end-->
-<!-- countdown timer -->
-<!-- divide the total time subtracting one interval from the end and play bell -->
+    <!-- countdown timer -->
+    <!-- divide the total time subtracting one interval from the end and play bell -->
+    <!-- congig button returns do the main page -->
+    <!-- <canvas width="2" height="2"></canvas> -->
 
-<div id="play-pause">
-    <img src="/src/assets/pause-icon.png" alt="pause button" /> 
-</div>
-<!-- <canvas width="2" height="2"></canvas> -->
+    <!-- change this to a circle or a timer. ref meditate typescript -->
 
 
+<template>
+  <div class="flex max-w-sm mx-auto">
+    <audio
+      id="meditationSound"
+      :src="`/bell-${selectedBell}.wav`"
+      type="audio/wav"
+      ref="audioPlayer"
+    >
+      Your browser does not support the audio element.
+    </audio>
+    <div class="pt-6 space-y-4" v-show="elapsed != undefined">
+      <p>{{ prettyTime }}</p>
+    </div>
+    <div class="text- font-medium text-black">
+      <button @click="play">play</button>
+      <button @click="pause">pause</button>
+      <button @click="stop">stop</button>
+    </div>
+  </div>
 </template>
 
 <script>
-
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: "meditate",
-    props: [ "time", "bell", "interval" ],
-    data() {
-        return{
-            totalTime: this.time,
-            selectedBell: this.bell,
-            totalIntervals: this.interval,
-        }
-    },
-    mounted(){
-        if ( this.selectedBell === undefined || this.totalTime === undefined|| this.totalIntervals === undefined) {
-            this.totalTime = 20;
-            this.selectedBell = 2;
-            this.totalIntervals= 4;
-        };
-        console.log(this.totalTime);
-        console.log(this.selectedBell);
-        console.log(this.totalIntervals);
-        
-    },
-    methods: {
-        initialBell() {
-            //play the bell at the beginning
-            //play interval bell(s)
-            //play ending bell
-            
-        }
+  name: "meditate",
+  props: ["time", "bell", "interval"],
+  data() {
+    return {
+      selectedBell: this.bell,
+      sound: undefined,
 
+      totalIntervals: this.interval,
+
+      totalTime: this.time,
+      startTime: undefined,
+      elapsed: undefined,
+    };
+  },
+  mounted() {
+    if (
+      this.selectedBell === undefined ||
+      this.totalTime === undefined ||
+      this.totalIntervals === undefined
+    ) {
+      this.totalTime = 3 * 60000;
+      this.selectedBell = 2;
+      this.totalIntervals = 1;
     }
-});
+    console.log(this.totalTime);
+    console.log(this.selectedBell);
+    console.log(this.totalIntervals);
+    console.log((this.totalTime/60000)/(this.totalIntervals +1));
+    this.sound = document.getElementById("meditationSound");
+    
+    
+  },
 
+  computed: {
+    prettyTime() {
+      return ((this.totalTime - this.elapsed) / 1000).toFixed(0);
+    },
+    
+  },
+
+  methods: {
+    //basic play, pause and stop functions
+
+    play() {
+      this.sound.play();
+      window.requestAnimationFrame(this.countdown);
+    },
+    pause() {
+      this.sound.pause();
+    },
+    stop() {
+      this.sound.pause();
+      this.sound.currentTime = 0;
+      this.sound.play();
+      console.log("paraparaparou");
+    },
+
+    countdown(timestamp) {
+      if (this.startTime === undefined) {
+        this.startTime = timestamp;
+      }
+      this.elapsed = timestamp - this.startTime;
+      if (this.elapsed <= this.totalTime) {
+        window.requestAnimationFrame(this.countdown);
+      } else {
+        this.stop();
+      }
+    },
+
+    //play interval bell(s)
+    //play ending bell
+  },
+});
 
 //background colors animation - transform to vue
 /* var canvas = document.querySelector('canvas');
@@ -86,18 +144,15 @@ function animate() {
 }
 
 animate(); */
-
 </script>
 
 <style scoped>
-
 body {
-    margin: 0 auto;
-    
+  margin: 0 auto;
 }
 
-
-/* html, body {
+html,
+body {
   height: 100%;
 }
 
@@ -112,5 +167,5 @@ canvas {
   width: 100%;
   height: 100%;
   transform: scale(2);
-} */
+}
 </style>
