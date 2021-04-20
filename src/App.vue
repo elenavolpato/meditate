9507fbc4fba5e18d93c958931763532136123a4b
Elena Volpato <talkto@elenavolpato.me>
2021-04-20 15:18:19 -0300

    fixed bg

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="m-0 bg-cover font-sans bg-gradient-to-r from-purple-400 via-pink-500 to-blue-500" id="app">
+<div class=" absolute inset-0 m-0 bg-cover font-sans bg-gradient-to-r from-purple-400 via-pink-500 to-blue-500" id="app">
   <router-view />
 </div>
 </template>
