6ce69ad6c7db0d3319053c9d5591a5f752c33a3c
Elena Volpato <talkto@elenavolpato.me>
2021-04-22 14:08:57 -0300

    added screen configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    },    
     fontFamily: {
       'sans': ['Helvetica', 'Arial', 'sans-serif'],
     },
