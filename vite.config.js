77751769770da8062824c6bed9fcab67b09b237e
Elena Volpato <talkto@elenavolpato.me>
2021-05-05 22:10:39 -0300

    fixed favicon configs

--- vite.config.js
+++ vite.config.js
@@ -9,23 +9,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      manifest: {
-          "name": "meditation timer",
-          "short_name": "meditate",
-          "lang": "en-US",
-          "start_url": "/",
-          "display": "standalone",
-          "theme_color": "#700c8c",
-          "icons": [
-            {
-              "src": "/icon.png",
-              "sizes": "192x142",
-              "type": "image/png"
-            }
-          ],
-          "background_color": "#700c8c",
-      },
-    })
+    
   ]
 })
