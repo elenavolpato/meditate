62a1369014803131153a6e3d8fcff9a55397a251
Elena Volpato <talkto@elenavolpato.me>
2021-05-22 17:14:01 -0300

    vite-PWAplugin test

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa' 
 
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      manifest: {
-          "name": "meditation timer",
-          "short_name": "meditation timer",
-          "lang": "en-US",
-          "start_url": "/",
-          "display": "standalone",
-          "description": "The perfect timer to keep track of your meditation.",
-          "background_color": "#610c8c",
-      },
-      /* workbox: {
-
-      } */
-    }),
+    VitePWA(),
     
   ]
 })
